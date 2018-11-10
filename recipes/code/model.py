e063caec18b9936fa3269f01f806ce52bdf9fc17
raphey <16931750+raphey@users.noreply.github.com>
2018-11-10 09:53:20 -0800

    python3 print statements

--- recipes/code/model.py
+++ recipes/code/model.py
@@ -5,16 +5,16 @@ train, test, validate = make_dataset()
 
 #get count of cuisines
 cuisineCounter = Counter(recipe['cuisine'] for recipe in train)
-print "~~~~~~~~~~~~~~CUISINES~~~~~~~~~~~~~~~~~~~~~~"
-print "{} cuisines found...".format(len(cuisineCounter))
+print("~~~~~~~~~~~~~~CUISINES~~~~~~~~~~~~~~~~~~~~~~")
+print("{} cuisines found...".format(len(cuisineCounter)))
 for k, v in cuisineCounter.most_common():
-    print "{}:  {} recipes".format(k, v)
+    print("{}:  {} recipes".format(k, v))
 
 #get count of ingredients
 total = [recipe['ingredients'] for recipe in train]
 total = [cat for sublist in total for cat in sublist] #flatten the list
 ingredientCounter = Counter(total)
-print "~~~~~~~~~~~~~~INGREDIENTS~~~~~~~~~~~~~~~~~~~~~~"
-print "{} ingredients found...".format(len(ingredientCounter))
+print("~~~~~~~~~~~~~~INGREDIENTS~~~~~~~~~~~~~~~~~~~~~~")
+print("{} ingredients found...".format(len(ingredientCounter)))
 for k, v in ingredientCounter.most_common():
-    print k, v
+    print(k, v)
