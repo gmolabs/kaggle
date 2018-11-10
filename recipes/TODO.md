409fef61aaa4e2998614d4c50091d1bdfd185241
Geoff Morris <geoffm@gmail.com>
2018-11-10 00:55:13 -0800

    count and list cuisines, count and list ingredients

--- recipes/TODO.md
+++ recipes/TODO.md
@@ -6,8 +6,8 @@ https://www.kaggle.com/c/whats-cooking-kernels-only
 - ~~download and parse (data.py) list of JSON~~
 - ~~make_dataset should return train, validate, test~~
 - ~~validate is a randomly selected 10% of train.json, pre-shuffled [validation_portion=0.1 could be another default arg] (see python random library)~~
-- histogram of ingredients (from collections import Counter)
-- count of recipes in each cuisine, count of cuisines
+- ~~histogram of ingredients (from collections import Counter)~~
+- ~~count of recipes in each cuisine, count of cuisines~~
 
 ## Modeling
 - task a
--- recipes/code/model.py
+++ recipes/code/model.py
@@ -1,5 +1,20 @@
 from data import make_dataset
+from collections import Counter
 
 train, test, validate = make_dataset()
 
-print(validate)
+#get count of cuisines
+cuisineCounter = Counter(recipe['cuisine'] for recipe in train)
+print "~~~~~~~~~~~~~~CUISINES~~~~~~~~~~~~~~~~~~~~~~"
+print "{} cuisines found...".format(len(cuisineCounter))
+for k, v in cuisineCounter.most_common():
+    print "{}:  {} recipes".format(k, v)
+
+#get count of ingredients
+total = [recipe['ingredients'] for recipe in train]
+total = [cat for sublist in total for cat in sublist] #flatten the list
+ingredientCounter = Counter(total)
+print "~~~~~~~~~~~~~~INGREDIENTS~~~~~~~~~~~~~~~~~~~~~~"
+print "{} ingredients found...".format(len(ingredientCounter))
+for k, v in ingredientCounter.most_common():
+    print k, v
