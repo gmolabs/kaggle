bc86b4ddcde6771281a19eb8db05b97ed7d034a2
Geoff Morris <geoffm@gmail.com>
2018-11-20 22:29:42 -0800

    trying other classifiers

--- recipes/TODO.md
+++ recipes/TODO.md
@@ -18,9 +18,42 @@ https://www.kaggle.com/c/whats-cooking-kernels-only
 - ~~create list of encoded recipes and labels for train data~~
 - ~~run and fit classifier~~
 - ~~print predictions and percent correct~~
-- print summary for a portion of training data equivalent in size to the validation set (to better monitor overfitting)
-- find the point at which additional ingredients are no longer helping the model
+- print summary for training data (to better monitor overfitting)
+- ~~find the point at which additional ingredients are no longer helping the model (400 ingredients: 66.6 correct, 800: 64% 2000: 66.7, 200: .612) 400 seems to be the sweet spot~~
 - try three different non-neural-net sklearn models to determine what performs best. Each model should be called via a short function in model.py
+
+linear kernal svc: Correct: 2611
+Incorrect: 1366
+Success rate: 0.6565250188584361
+
+random forest (much faster than svc):
+Correct: 1015
+Incorrect: 2962
+Success rate: 0.25521750062861454
+
+adaboost:
+Correct: 2046
+Incorrect: 1931
+Success rate: 0.5144581342720643
+
+RandomForestClassifier(max_depth=20, n_estimators=200, max_features='sqrt'):
+Correct: 2216
+Incorrect: 1761
+Success rate: 0.5572039225546894
+
+GradientBoostingClassifier:
+Correct: 2564
+Incorrect: 1413
+Success rate: 0.6447070656273574
+
+GradientBoostingClassifierTuned:
+Correct: 2738
+Incorrect: 1239
+Success rate: 0.6884586371636913
+
+
+
+
 - modify the sklearn functions to have a save parameter which pickles the model once it's been trained. These models to save should also have probability set to True, to give more interesting info when used in the Presentation tasks.
 - Figure out how to pickle and unpickle one-hot feature encoders, since the model pickling isn't too useful without knowing the associated encoder. One solution is for the models to be passed unencoded data and the parameters with which to encode, then they can create the one-hot encoder and save it alongside the model itself. (One-hot cuisine encoding will be the same every time and can be done from scratch every time.)
 - Write a function which unpickles and returns any of the sklearn models plus an encoder, given a path to a pickled model and a path to an encoder.
--- recipes/code/model.py
+++ recipes/code/model.py
@@ -6,27 +6,50 @@ from sklearn import svm, metrics
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
 from utils import indexInList
+#classifiers
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 
-
+classifierIndex = 12
 
 ingredientCutoff = 400
 train, test, validate = make_dataset()
 
-#get count of cuisines
-#cuisineCounter = Counter(recipe['cuisine'] for recipe in train)
-#print("~~~~~~~~~~~~~~CUISINES~~~~~~~~~~~~~~~~~~~~~~")
-#print("{} cuisines found...".format(len(cuisineCounter)))
-#for k, v in cuisineCounter.most_common():
-#    print("{}:  {} recipes".format(k, v))
+names = ["Nearest Neighbors", "Linear SVM", "RBF SVM", "Scale SVC" "Gaussian Process",
+         "Decision Tree", "Random Forest", "Neural Net", "AdaBoost",
+         "Naive Bayes", "QDA", "GradientBoostingClassifier", "GradientBoostingClassifierTuned"]
+
+classifiers = [
+    KNeighborsClassifier(3),
+    SVC(kernel="linear", C=0.025),
+    SVC(gamma=2, C=1),
+    SVC(gamma='scale'),
+    GaussianProcessClassifier(1.0 * RBF(1.0)),
+    DecisionTreeClassifier(max_depth=5),
+    RandomForestClassifier(max_depth=20, n_estimators=200, max_features='sqrt'),
+    MLPClassifier(alpha=1),
+    AdaBoostClassifier(),
+    GaussianNB(),
+    QuadraticDiscriminantAnalysis(),
+    GradientBoostingClassifier(),
+    GradientBoostingClassifier(n_estimators=200, learning_rate=.2, max_depth=5, max_features='sqrt')]
+
 
 #get count of ingredients
 total = [recipe['ingredients'] for recipe in train]
 total = [cat for sublist in total for cat in sublist] #flatten the list
 ingredientCounter = Counter(total)
-print("~~~~~~~~~~~~~~INGREDIENTS~~~~~~~~~~~~~~~~~~~~~~")
-print("{} ingredients found...".format(len(ingredientCounter)))
-for k, v in ingredientCounter.most_common():
-    print("{}:  {}".format(k, v))
+#print("~~~~~~~~~~~~~~INGREDIENTS~~~~~~~~~~~~~~~~~~~~~~")
+#print("{} ingredients found...".format(len(ingredientCounter)))
+#for k, v in ingredientCounter.most_common():
+#    print("{}:  {}".format(k, v))
 
 #print("~~~~~~~~~~~~~~TOP INGREDIENTS~~~~~~~~~~~~~~~~~~~~~~")
 #print("Showing the top {} most common ingredients...".format(ingredientCutoff))
@@ -36,6 +59,18 @@ for k, _ in topIngredientsCount:
 #    print("{}:  {} uses".format(k, v))
     ingredientNames.append(k)
 #print(ingredientNames)
+
+
+
+
+#get count of cuisines
+#cuisineCounter = Counter(recipe['cuisine'] for recipe in train)
+#print("~~~~~~~~~~~~~~CUISINES~~~~~~~~~~~~~~~~~~~~~~")
+#print("{} cuisines found...".format(len(cuisineCounter)))
+#for k, v in cuisineCounter.most_common():
+#    print("{}:  {} recipes".format(k, v))
+
+
 
 #create recipe vectors of 1hot-encoded ingredients (binary vecs with a 1 at the index of each ingredient)
 #onehotIngredients = []
@@ -57,28 +92,57 @@ cuisineLabels = list(recipe["cuisine"] for recipe in train)
 expected = list(recipe["cuisine"] for recipe in validate)
 
 #setup classifier and fit data
-classifier = svm.SVC(gamma='scale')
+#classifier = svm.SVC(gamma='scale')
+classifier = classifiers[classifierIndex]
 classifier.fit(onehotTrainingRecipes, cuisineLabels)
 
 #predict cuisine from onehotRecipes
 #print("predition should be:")
 #print(expected)
+
 predicted = classifier.predict(onehotValidationRecipes)
+print("Validation set results:")
+print("************************************")
 
 print("Classification report for classifier %s:\n%s\n"
       % (classifier, metrics.classification_report(expected, predicted)))
-#print("Confusion matrix:\n%s" % metrics.confusion_matrix(expected, predicted))
+print("Confusion matrix:\n%s" % metrics.confusion_matrix(expected, predicted))
 
 correctGuesses = 0;
 for index,prediction in enumerate(predicted):
-    print("Actual/Predicted: {}/{}".format(expected[index], prediction))
+#    print("Actual/Predicted: {}/{}".format(expected[index], prediction))
     if(expected[index]==prediction):
         correctGuesses+=1
 print("Correct: {}".format(correctGuesses))
 print("Incorrect: {}".format((len(predicted)-correctGuesses)))
 print("Success rate: {}".format(correctGuesses/len(predicted)))
 
+
 #portion = classifier.predict(onehotPortion)
 #print("Checking portion of train data for overfitting. Report for classifier %s:\n%s\n"
 #      % (classifier, metrics.classification_report(expected, predicted)))
 #print("Confusion matrix:\n%s" % metrics.confusion_matrix(expected, predicted))
+
+
+#predict cuisine from onehotRecipes
+#print("predition should be:")
+#print(expected)
+
+
+
+#predicted = classifier.predict(onehotTrainingRecipes)
+#print("Training set (Overfit reference) results:")
+#print("*****************************************")
+
+#print("Classification report for classifier %s:\n%s\n"
+#      % (classifier, metrics.classification_report(cuisineLabels, predicted)))
+#print("Confusion matrix:\n%s" % metrics.confusion_matrix(cuisineLabels, predicted))
+
+#correctGuesses = 0;
+#for index,prediction in enumerate(predicted):
+#    print("Actual/Predicted: {}/{}".format(expected[index], prediction))
+#    if(cuisineLabels[index]==prediction):
+#        correctGuesses+=1
+#print("Correct: {}".format(correctGuesses))
+#print("Incorrect: {}".format((len(predicted)-correctGuesses)))
+#print("Success rate: {}".format(correctGuesses/len(predicted)))
